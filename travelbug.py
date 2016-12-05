4d1d4baa48dd4fb41023b45a0292a0fb4295789b
Quoc Lieu <qlieu@student.unimelb.edu.au>
2016-12-05 23:44:47 +1100

    day page headings

--- travelbug.py
+++ travelbug.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from gevent.wsgi import WSGIServer
+# from gevent.wsgi import WSGIServer
 app = Flask(__name__, static_folder='.', static_url_path='')
 
 #import all routes from routes directory
