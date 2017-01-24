94bcb35655ebbec588bacdcfb4a6927086b0d07e
Zhijin Li <Zhijin Li>
2017-01-24 13:23:27 +1100

    make menu slide down

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,22 +1,13 @@
-function headMenu() {
-    var x = document.getElementById("rightnav");
-    var y = document.getElementById("top-nav-line");
-
-    if (x.className === "") {
-        x.className = "responsive";
-        y.className="";
-    } else {
-        x.className = "";
-        y.className="vertical-line";
-    }
-}
 
 
-function footMenu() {
-    var x = document.getElementById("links");
-    if (x.className === "") {
-        x.className = "responsive";
-    } else {
-        x.className = "";
-    }
-}
+$(document).ready(function(){
+    $("#head-bars").click(function(){
+        $("#responsive-head").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#footer-bars").click(function(){
+        $("#responsive-footer").slideToggle("slow");
+    });
+});
